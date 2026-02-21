6488aacbfe091faa569eef349c353d460d4e1424
balanced0 <alvichowdhury013@gmail.com>
2026-02-21 17:51:48 +0600

    Completed the functionality of the cashout section

--- script/cashout.js
+++ script/cashout.js
@@ -1,1 +1,30 @@
-document.getElementById("cashout-btn").addEventListener("click", function(){});
+document.getElementById("cashout-btn").addEventListener("click", function(){
+    const agentInput = document.getElementById("agent-number-input");
+    const agentNumber = agentInput.value;
+
+    const withdrawAmountInput = document.getElementById("cashout-amount-input");
+    const withdrawAmount = withdrawAmountInput.value;
+
+    const pinInput = document.getElementById("cashout-pin-input");
+    const pin = pinInput.value;
+
+    if(agentNumber.length < 11 || agentNumber.length > 11){
+        alert("Invalid Agent Number");
+        return;
+    }
+    
+    const currentBalanceInput = document.getElementById("balance");
+    const currentBalance = currentBalanceInput.innerText;
+    const newBalance = Number(currentBalance) - Number(withdrawAmount);
+    if(newBalance < 0){
+        alert("You are too poor to withdraw that much");
+        return;
+    }
+    if(pin === "1234"){
+        currentBalanceInput.innerText = newBalance;
+    }
+    else{
+        alert("Invalid Pin");
+        return;
+    }
+});
