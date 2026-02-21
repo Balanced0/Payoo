2281ccea112fa054bb981599679c4b7a62a69993
balanced0 <alvichowdhury013@gmail.com>
2026-02-21 20:02:26 +0600

    Created some more common functions to reduce repitations

--- script/machine.js
+++ script/machine.js
@@ -3,3 +3,11 @@ function getValueFromInput(id){
     const value = input.value;
     return value;
 }
+function getBalance(){
+    const currentBalanceInput = document.getElementById("balance");
+    const currentBalance = currentBalanceInput.innerText;
+    return Number(currentBalance);
+}
+function setBalance(value){
+    document.getElementById("balance").innerText = value;
+}
