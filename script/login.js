1f9adaffb5bd058b77389d4d37cfbf81f5879ba6
balanced0 <alvichowdhury013@gmail.com>
2026-02-21 11:32:41 +0600

    Completed the index page functionality

--- script/login.js
+++ script/login.js
@@ -1,1 +1,21 @@
-console.log("Successfully loaded");
+document.getElementById("btn-id").addEventListener("click", function(){
+    //getting the phone number
+    const numberInput = document.getElementById("number-input");
+    const phoneNumber = numberInput.value;
+
+    //getting the pin number
+    const pinInput = document.getElementById("pin-input");
+    const pinNumber = pinInput.value;
+
+    //phone and pin number matching
+    if(phoneNumber === "01234567890" && pinNumber === "1234"){
+        //if matched send to home page
+        alert("login Successful");
+        window.location.assign("../home.html");
+    }
+    else{
+        //else alert login failed
+        alert("Login Failed");
+        return;
+    }
+})
